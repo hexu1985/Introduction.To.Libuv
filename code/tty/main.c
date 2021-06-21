32b46155d710c08b5eae59bb44efd2db23cd251c
hexu <hexu_1985@sina.com>
2021-06-21 10:40:09 +0800

    解决tty/main.c的崩溃问题

--- code/tty/main.c
+++ code/tty/main.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <uv.h>
+#include <stdlib.h>
 
 uv_loop_t *loop;
 uv_tty_t tty;
@@ -12,18 +13,18 @@ int main() {
     uv_tty_set_mode(&tty, UV_TTY_MODE_NORMAL);
     
     if (uv_guess_handle(1) == UV_TTY) {
-        uv_write_t req;
+        uv_write_t *req = (uv_write_t *) malloc(sizeof(uv_write_t));
         uv_buf_t buf;
         buf.base = "\033[41;37m";
         buf.len = strlen(buf.base);
-        uv_write(&req, (uv_stream_t*) &tty, &buf, 1, NULL);
+        uv_write(req, (uv_stream_t*) &tty, &buf, 1, NULL);
     }
 
-    uv_write_t req;
+    uv_write_t *req = (uv_write_t *) malloc(sizeof(uv_write_t));
     uv_buf_t buf;
     buf.base = "Hello TTY\n";
     buf.len = strlen(buf.base);
-    uv_write(&req, (uv_stream_t*) &tty, &buf, 1, NULL);
+    uv_write(req, (uv_stream_t*) &tty, &buf, 1, NULL);
     uv_tty_reset_mode();
     return uv_run(loop, UV_RUN_DEFAULT);
 }
